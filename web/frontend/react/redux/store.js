779cabc38245b234d5c4922628bb14912eb5c402
Michal Podworny <michal.podworny@monterail.com>
2016-04-19 21:24:04 +0200

    Clean up after testing Redux

--- web/frontend/react/redux/store.js
+++ web/frontend/react/redux/store.js
@@ -2,12 +2,10 @@ import { createStore as _createStore, combineReducers, applyMiddleware, compose 
 import thunkMiddleware from "redux-thunk"
 import { routerReducer } from "react-router-redux"
 import DevTools from "components/DevTools"
-import testReducer from "./reducers/test"
 
 export default function createStore() {
   const middleware = [thunkMiddleware]
   const reducer = combineReducers({
-    test: testReducer,
     routing: routerReducer
   })
   const enhancer = compose(applyMiddleware(...middleware), DevTools.instrument())
--- web/frontend/react/views/Home.js
+++ web/frontend/react/views/Home.js
@@ -1,44 +1,16 @@
 import React from "react"
 import { Component } from "react"
 import "./Home.scss"
-import Test from "components/Test"
-import TestTwo from "components/TestTwo"
-import { connect } from "react-redux"
-import TestActions from "redux/actions/test"
 
 class Home extends Component {
-  collection = [1, 2, 3]
-
-  clickCallback = () => {
-    console.log(this)
-  }
-
-  addItem = () => {
-    this.props.addItem({ id: new Date().getTime(), text: "test" })
-  }
-
   render() {
     return (
       <div>
         <div className="">
-          {
-            this.props.test.map((item) => {
-              return (
-                <TestTwo key={item.id} item={item} removeItem={this.props.removeItem}></TestTwo>
-              )
-            })
-          }
-        </div>
-        <div onClick={this.addItem}>
-          Add item
         </div>
       </div>
     )
   }
 }
 
-function mapStateToProps(state) {
-  return { test: state.test }
-}
-
-export default connect(mapStateToProps, TestActions)(Home)
+export default Home
