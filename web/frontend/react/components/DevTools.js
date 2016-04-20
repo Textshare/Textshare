b6a9642cd63515f903b7d5f5d87e3f2b3118a1f7
Michal Podworny <michal.podworny@monterail.com>
2016-04-20 20:13:40 +0200

    Add Dispatcher do DevTools

--- web/frontend/react/components/DevTools.js
+++ web/frontend/react/components/DevTools.js
@@ -4,10 +4,15 @@ import { createDevTools } from "redux-devtools";
 
 import LogMonitor from "redux-devtools-log-monitor";
 import DockMonitor from "redux-devtools-dock-monitor";
+import Dispatcher from "redux-devtools-dispatch"
+import MultipleMonitors from "redux-devtools-multiple-monitors"
 
 const DevTools = createDevTools(
   <DockMonitor toggleVisibilityKey="ctrl-h" changePositionKey="ctrl-q" defaultIsVisible={false}>
-    <LogMonitor theme="tomorrow" />
+    <MultipleMonitors>
+      <LogMonitor theme="tomorrow" />
+      <Dispatcher />
+    </MultipleMonitors>
   </DockMonitor>
 );
 
