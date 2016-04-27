6db2b708d77c5168d043de4da802d44a3e3faed8
Michal Podworny <michal.podworny@monterail.com>
2016-04-27 19:30:36 +0200

    Allow to jump between rows with left-right arrows

--- web/frontend/react/components/editor_input_handler.js
+++ web/frontend/react/components/editor_input_handler.js
@@ -6,7 +6,23 @@ function keyUpHandler(event, rowId, editor) {
 }
 
 function keyDownHandler(event, rowId, editor) {
-
+  // right arrow
+  if (event.keyCode === 39) {
+    let currentRowIndex = editor.rows.findIndex((row) => { return row.id === rowId })
+    if (window.getSelection().anchorOffset === editor.rows[currentRowIndex].text.length &&
+    currentRowIndex + 1 < editor.rows.length) {
+      editor.setCursorPosition(0)
+      editor.setFocusedRow(editor.rows[currentRowIndex + 1].id)
+    }
+  }
+  // left arrow
+  if (event.keyCode === 37) {
+    let currentRowIndex = editor.rows.findIndex((row) => { return row.id === rowId })
+    if (window.getSelection().anchorOffset === 0 && currentRowIndex > 0) {
+      editor.setCursorPosition(editor.rows[currentRowIndex - 1].text.length)
+      editor.setFocusedRow(editor.rows[currentRowIndex - 1].id)
+    }
+  }
 }
 
 function onClickHandler(event, rowId, editor) {
