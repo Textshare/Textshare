6e117cf27a7b1b117f2f8446aa324ba1d6623ed2
Michal Podworny <michal.podworny@monterail.com>
2016-05-01 18:40:42 +0200

    Change the up/down handling to the same as left/right

--- web/frontend/react/components/editor_input_handler.js
+++ web/frontend/react/components/editor_input_handler.js
@@ -14,32 +14,8 @@ function cursorPositionAfterJump(desiredPosition, row) {
 }
 
 function keyUpHandler(event, rowId, editor) {
-  // down arrow
-  if (event.keyCode === 40) {
-    let currentRowIndex = editor.rows.findIndex((row) => { return row.id === rowId })
-    if (cursorShouldJumpToNextLine(currentRowIndex, editor)) {
-      let nextRow = editor.rows[currentRowIndex + 1]
-      let newCursorPosition = cursorPositionAfterJump(event.previousCursorPosition, nextRow)
-      editor.setCursorPosition(newCursorPosition)
-      editor.setFocusedRow(nextRow.id)
-    } else {
-      editor.setCursorPosition(window.getSelection().anchorOffset)
-    }
-  }
-  // up arrow
-  if (event.keyCode === 38) {
-    let currentRowIndex = editor.rows.findIndex((row) => { return row.id === rowId })
-    if (cursorShouldJumpToPreviousLine(currentRowIndex, editor)) {
-      let previousRow = editor.rows[currentRowIndex - 1]
-      let newCursorPosition = cursorPositionAfterJump(event.previousCursorPosition, previousRow)
-      editor.setCursorPosition(newCursorPosition)
-      editor.setFocusedRow(previousRow.id)
-    } else {
-      editor.setCursorPosition(window.getSelection().anchorOffset)
-    }
-  }
-  // left/right arrows
-  if ([37, 39].includes(event.keyCode)) {
+  // arrows
+  if ([37, 38, 39, 40].includes(event.keyCode)) {
     editor.setCursorPosition(window.getSelection().anchorOffset)
   }
   // enter
@@ -49,8 +25,8 @@ function keyUpHandler(event, rowId, editor) {
 }
 
 function keyDownHandler(event, rowId, editor) {
-  // right arrow
-  if (event.keyCode === 39) {
+  // right/down arrows
+  if ([39, 40].includes(event.keyCode)) {
     let currentRowIndex = editor.rows.findIndex((row) => { return row.id === rowId })
     if (cursorShouldJumpToNextLine(currentRowIndex, editor)) {
       event.preventDefault()
@@ -58,18 +34,14 @@ function keyDownHandler(event, rowId, editor) {
       editor.setFocusedRow(editor.rows[currentRowIndex + 1].id)
     }
   }
-  // left arrow
-  if (event.keyCode === 37) {
+  // left/up arrows
+  if ([37, 38].includes(event.keyCode)) {
     let currentRowIndex = editor.rows.findIndex((row) => { return row.id === rowId })
     if (cursorShouldJumpToPreviousLine(currentRowIndex, editor)) {
       event.preventDefault()
       editor.setCursorPosition(editor.rows[currentRowIndex - 1].text.length)
       editor.setFocusedRow(editor.rows[currentRowIndex - 1].id)
     }
-  }
-  // up/down arrows
-  if ([38, 40].includes(event.keyCode)) {
-    event.previousCursorPosition = window.getSelection().anchorOffset
   }
   // backspace
   if (event.keyCode === 8) {
