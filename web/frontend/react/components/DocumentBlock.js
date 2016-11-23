cc7f5b18f6f5d717c1aa367c540acf3ec802f755
Michal Podworny <michal.podworny@monterail.com>
2016-11-23 20:22:32 +0100

    Add metadata to document list

--- web/frontend/react/components/DocumentBlock.js
+++ web/frontend/react/components/DocumentBlock.js
@@ -2,14 +2,30 @@ import React from "react"
 import { Component } from "react"
 import { Link } from "react-router"
 import "./DocumentBlock.scss"
+import moment from "moment"
 
 class DocumentBlock extends Component {
   removeDocument = () => { this.props.removeDocument(this.props.document) }
+  formatDate = (date) => { return moment(date).format("YYYY-MM-DD HH:mm:ss") }
 
   render() {
     return (
       <div className="document-block">
         <div>
+          <div className="document-block-metadata col-xs-12">
+            <span className="document-block-metadata-item">
+              {"Created by: " + this.props.document.owner.name}
+            </span>
+            <span className="document-block-metadata-item">
+              {"Created at: " + this.formatDate(this.props.document.inserted_at)}
+            </span>
+            <span className="document-block-metadata-item">
+              {"Updated at: " + this.formatDate(this.props.document.updated_at)}
+            </span>
+            <span className="document-block-metadata-item">
+              {"Tags: " + this.props.document.tags.map((tag) => { return tag.name }).join(", ")}
+            </span>
+          </div>
           <div className="document-block-title col-xs-11">
             <Link to={"/docs/" + this.props.document.id}>
               {this.props.document.title}
