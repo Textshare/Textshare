9e8029b89d5f00760e6d26befd54cb01265661cb
Michal Podworny <michal.podworny@monterail.com>
2016-12-13 00:00:55 +0100

    Add logic for appending content to document

--- web/frontend/react/views/DocumentView.js
+++ web/frontend/react/views/DocumentView.js
@@ -1,12 +1,16 @@
 import React from "react"
 import { Component } from "react"
+import { connect } from "react-redux"
 import "./DocumentView.scss"
 import Editor from "components/Editor"
+import * as DocumentsActions from "redux/actions/documents"
+import * as EditorActions from "redux/actions/editor"
 import TagList from "components/TagList"
 import { Link } from "react-router"
-import { httpPost } from "utils"
+import { httpPost, httpGet } from "utils"
 import Collaborators from "components/Collaborators"
 import Modal from "react-modal"
+import Select from "react-select"
 
 class DocumentView extends Component {
   modalStyles = {
@@ -41,16 +45,53 @@ class DocumentView extends Component {
     httpPost("/api/v1/documents/" + this.props.params.documentId + "/revisions")
   }
 
+  selectDocumentToAppend = (selection) => {
+    this.setState({ selectedDocumentToAppend: selection ? selection.value : null })
+  }
+
+  documentsToDropdownOptions = (docs) => {
+    return docs.filter((doc) => {
+      return doc.id != this.props.params.documentId
+    }).map((doc) => {
+      return { value: doc, label: doc.title }
+    })
+  }
+
   openAppendDocumentModal = () => {
     this.setState({ appendDocumentModalOpen: true })
+    httpGet("/api/v1/documents").then((documents) => {
+      this.setState({ availableDocumentsToAppend: documents })
+    })
+  }
+
+  confirmAppendDocument = () => {
+    if (!this.state.selectedDocumentToAppend) {
+      return;
+    } else {
+      let newContent = this.props.editedDocument.content + "\n" +
+        this.state.selectedDocumentToAppend.content
+      let newRowIds = this.props.editedDocument.row_ids
+        .concat(this.state.selectedDocumentToAppend.row_ids)
+
+      this.props.setContent(this.props.params.documentId, newContent)
+      this.props.setRowIds(this.props.params.documentId, newRowIds)
+
+      this.props.updateDocument(this.props.params.documentId)
+
+      this.closeAppendDocumentModal()
+    }
   }
 
   closeAppendDocumentModal = () => {
-    this.setState({ appendDocumentModalOpen: false })
+    this.setState({
+      appendDocumentModalOpen: false, availableDocumentsToAppend: [], selectedDocumentToAppend: null
+    })
   }
 
   componentWillMount() {
-    this.setState({ appendDocumentModalOpen: false })
+    this.setState({
+      appendDocumentModalOpen: false, availableDocumentsToAppend: [], selectedDocumentToAppend: null
+    })
   }
 
   // It seems like it can't be commented out in the render function leaving it here.
@@ -64,11 +105,27 @@ class DocumentView extends Component {
         <Modal
           isOpen={this.state.appendDocumentModalOpen}
           onRequestClose={this.closeAppendDocumentModal}
-          contentLabel="Example Modal"
+          contentLabel="Append other document's contents to this one"
           style={this.modalStyles}
         >
-          <span>Ziemniaki</span>
-          <button onClick={this.closeAppendDocumentModal}>Close</button>
+          <div>
+            <span>Choose the document to append to the currently opened one</span>
+          </div>
+          <div>
+            <Select
+              name="appended-document"
+              value={this.state.selectedDocumentToAppend}
+              valueRenderer={function (option) { return option.title }}
+              options={this.documentsToDropdownOptions(this.state.availableDocumentsToAppend)}
+              onChange={this.selectDocumentToAppend}
+            ></Select>
+          </div>
+          <div>
+            <button onClick={this.confirmAppendDocument}>Append</button>
+          </div>
+          <div>
+            <button onClick={this.closeAppendDocumentModal}>Close</button>
+          </div>
         </Modal>
 
         <Collaborators documentId={this.props.params.documentId}></Collaborators>
@@ -80,4 +137,10 @@ class DocumentView extends Component {
   }
 }
 
-export default DocumentView
+function mapStateToProps(state, props) {
+  return {
+    editedDocument: state.documents[props.params.documentId],
+  }
+}
+
+export default connect(mapStateToProps, Object.assign({}, EditorActions, DocumentsActions))(DocumentView)
